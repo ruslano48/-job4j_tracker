ec29ba0cec3f3bbfb7acd6e9a732822396b938a6
user <ruslano48@yandex.ru>
2024-08-16 11:15:23 +0300

    Репозиторий поод проект Tracker

---
+++ src/main/java/ru/job4j/Checkstyle/Broken.java
@@ -0,0 +1,22 @@
+public class Broken {
+    private final int sizeOfEmpty = 10;
+
+    public String surname;
+    public static final String NEW_VALUE = "";
+
+    private String name;
+
+    public Broken() {
+    }
+
+    void echo() {
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g, int h) {
+    }
+}
