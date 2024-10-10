17f6b28c3667abd220ef1347547d924278e100c3
user <ruslano48@yandex.ru>
2024-10-10 16:31:59 +0300

    Репозиторий поод проект Tracker

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,7 +1,31 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String food) {
+        this.food = food;
+    }
+
+    public void show() {
+        System.out.println("Котик " + this.name + " ел " + this.food);
+    }
+
     public static void main(String[] args) {
+
         Cat peppy = new Cat();
+
+        peppy.giveNick("Peppy");
+
+        peppy.eat("рыбу");
+
+        peppy.show();
     }
 }
