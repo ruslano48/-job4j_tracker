f7b49bf6ccea7128a7e2ad88dbb2807f6e41120d
user <ruslano48@yandex.ru>
2024-10-11 12:52:36 +0300

    Репозиторий поод проект Tracker

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,34 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        this.active = false;
+        this.status = 0;
+        this.message = "No Error";
+
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + active);
+        System.out.println("Status: " + status);
+        System.out.println("Message: " + message);
+    }
+
+    public static void main(String[] args) {
+        Error defaultError = new Error();
+        Error customError = new Error(true, 404, "Page not found");
+
+        defaultError.printInfo();
+        customError.printInfo();
+    }
+}
