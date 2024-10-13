2755a8a084711033b2cbb2b3520b322ba12d070a
user <ruslano48@yandex.ru>
2024-10-13 08:56:41 +0300

    Репозиторий поод проект Tracker

--- src/main/java/ru/job4j/oop/Calculator.java
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -7,7 +7,37 @@ public class Calculator {
         return x + y;
     }
 
+    public int multiply(int a) {
+        return x * a;
+    }
+
+    public static int minus(int y) {
+        return y - x;
+    }
+
+    public int divide(int a) {
+        return a / x;
+    }
+
+    public int sumAllOperation(int a) {
+        int sumResult = sum(a);
+        int multiplyResult = multiply(a);
+        int minusResult = minus(a);
+        int divideResult = divide(a);
+        return sumResult + minusResult + minusResult + divideResult;
+    }
+
     public static void main(String[] args) {
-        
+        int result1 = Calculator.sum(10);
+        System.out.println("Result of sum: " + result1);
+        int result2 = Calculator.minus(10);
+        System.out.println("Result of minus: " + result2);
+        Calculator calculator = new Calculator();
+        int result3 = calculator.multiply(5);
+        System.out.println("Result of multiply: " + result3);
+        int result4 = calculator.divide(10);
+        System.out.println("Result of divide: " + result4);
+        int result5 = calculator.sumAllOperation(10);
+        System.out.println("Result of sumAllOperation: " + result5);
     }
 }
