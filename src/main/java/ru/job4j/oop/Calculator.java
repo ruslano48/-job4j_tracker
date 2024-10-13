e3842613f4cffa2e9d1ee0136d9e252e8c024e2d
user <ruslano48@yandex.ru>
2024-10-13 07:18:40 +0300

    Репозиторий поод проект Tracker

---
+++ src/main/java/ru/job4j/oop/Calculator.java
@@ -0,0 +1,13 @@
+package ru.job4j.oop;
+
+public class Calculator {
+    private static int x = 5;
+
+    public static int sum(int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
