98a731239b411586f01fbb9d6cc5efc0ac111592
user <ruslano48@yandex.ru>
2024-10-13 10:33:18 +0300

    Репозиторий поод проект Tracker

---
+++ src/test/java/ru/job4j/oop/TriangleTest.java
@@ -0,0 +1,51 @@
+package ru.job4j.oop;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+public class TriangleTest {
+
+    @Test
+    public void when00and40and04Then8() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = 8;
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when00and20and02Then2() {
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        Point c = new Point(0, 2);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = 2;
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void when00and40and04ThenAreaIs8() {
+        Point a = new Point(0, 0);
+        Point b = new Point(4, 0);
+        Point c = new Point(0, 4);
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = 8;
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    }
+
+    @Test
+    public void whenCollinearPointsThenAreaIsMinusOne() {
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 1);
+        Point c = new Point(2, 2); // Все точки лежат на одной прямой
+        Triangle triangle = new Triangle(a, b, c);
+        double result = triangle.area();
+        double expected = -1; // Треугольник не существует
+        assertThat(result).isCloseTo(expected, offset(0.001));
+    }
+}
