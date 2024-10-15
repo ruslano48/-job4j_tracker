ea5ce4c29f30a249a501070253dc430a845cab61
user <ruslano48@yandex.ru>
2024-10-15 11:45:58 +0300

    Репозиторий поод проект Tracker

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -1,14 +1,11 @@
 package ru.job4j.inheritance;
-
 public class JSONReport extends TextReport {
-        @Override
-        public String generate(String name, String body) {
-            return "{" + System.lineSeparator()
-                    +
-                    "  \"name\" : \"" + name + "\"," + System.lineSeparator()
-                    +
-                    "  \"body\" : \"" + body + "\"" + System.lineSeparator()
-                    +
-                    "}";
-        }
+    @Override
+    public String generate(String name, String body) {
+        String ln = System.lineSeparator();
+        return "{" + ln
+                + "\t\"name\" : \"" + name + "\"," + ln
+                + "\t\"body\" : \"" + body + "\"" + ln
+                + "}";
     }
+}
--- src/test/java/ru/job4j/inheritance/JSONReportTest.java
+++ src/test/java/ru/job4j/inheritance/JSONReportTest.java
@@ -1,4 +1,5 @@
 package ru.job4j.inheritance;
+
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
