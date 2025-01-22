f831e7e5a354f5f252570ab145c211056c484363
user <ruslano48@yandex.ru>
2025-01-22 16:24:35 +0300

    Репозиторий поод проект Tracker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -53,4 +53,14 @@ public class Tracker {
         }
         return false;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index == -1) {
+            return;
+        }
+        System.arraycopy(items, index + 1, items, index, size - index - 1);
+        items[size - 1] = null;
+        size--;
+    }
 }
