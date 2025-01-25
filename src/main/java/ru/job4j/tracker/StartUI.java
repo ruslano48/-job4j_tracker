dedc9aee7476507a4e607831d121ca789ea7f46f
user <ruslano48@yandex.ru>
2025-01-25 09:48:46 +0300

    Репозиторий поод проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,20 +1,45 @@
 package ru.job4j.tracker;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class StartUI {
+
+    public void init(Scanner scanner, Tracker tracker) {
+        boolean run = true;
+        while (run) {
+            showMenu();
+            System.out.println("Выбрать: ");
+        int select = Integer.parseInt(scanner.nextLine());
+        if (select != 6) {
+            System.out.println("Пользователь выбрал: " + select);
+
+        } else {
+            System.out.println("Программа завершена. ");
+            run = false;
+
+        }
+    }
+}
+
+    private void showMenu() {
+        String[] menu = {
+                "Добавить новую заявку",
+                "Показать все заявки",
+                "Изменить заявку",
+                "Удалить заявку",
+                "Показать заявку по id",
+                "Показать заявки по имени",
+                "Завершить программу"
+        };
+        System.out.println("Меню:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " +  menu[i]);
+        }
+    }
+
     public static void main(String[] args) {
-        Item item = new Item("New Item");
-        Item item1 = new Item(1, "New item");
-
-        LocalDateTime createdDate = item.getCreated();
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-
-        String formattedDate = createdDate.format(formatter);
-        System.out.println("Item created on: " + formattedDate);
-        System.out.println(item);
-        System.out.println(item1);
+        Scanner scanner = new Scanner(System.in);
+        Tracker tracker = new Tracker();
+        new StartUI().init(scanner, tracker);
     }
 }
