ccc3ea84a79dc8eaf9b37a98c1aafee442a46447
user <ruslano48@yandex.ru>
2025-02-01 09:02:45 +0300

    Репозиторий поод проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -9,17 +9,19 @@ public class StartUI {
         while (run) {
             showMenu();
             System.out.print("Выбрать: ");
-        int select = Integer.parseInt(scanner.nextLine());
-        if (select != 6) {
-            System.out.println("Пользователь выбрал: " + select);
-
-        } else {
-            System.out.println("Программа завершена. ");
-            run = false;
-
+            int select = Integer.parseInt(scanner.nextLine());
+            if (select == 0) {
+                System.out.println("=== Создание новой заявки ===");
+                System.out.print("Введите имя: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                tracker.add(item);
+                System.out.println("Добавленная заявка: " + item);
+            } else if (select == 6) {
+                run = false;
+            }
         }
     }
-}
 
     private void showMenu() {
         String[] menu = {
@@ -33,7 +35,7 @@ public class StartUI {
         };
         System.out.println("Меню:");
         for (int i = 0; i < menu.length; i++) {
-            System.out.println(i + ". " +  menu[i]);
+            System.out.println(i + ". " + menu[i]);
         }
     }
 
@@ -42,5 +44,4 @@ public class StartUI {
         Tracker tracker = new Tracker();
         new StartUI().init(scanner, tracker);
     }
-
 }
