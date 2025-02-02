0ea5842c525a6467a5ba904d0cd243cf643e7593
user <ruslano48@yandex.ru>
2025-02-02 08:46:33 +0300

    Репозиторий поод проект Tracker

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -46,7 +46,7 @@ public class StartUI {
 
             } else if (select == 4) {
                 System.out.println("=== Вывод заявки по id: ");
-                System.out.println("Введите id: ");
+                System.out.print("Введите id: ");
                 int id = Integer.parseInt(scanner.nextLine());
                 Item item = tracker.findById(id);
                 if (item != null) {
