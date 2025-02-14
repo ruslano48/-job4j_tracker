c8bff39fc5fafb56e7e8d4389286786a8dc221ee
user <ruslano48@yandex.ru>
2025-02-14 12:49:10 +0300

    Репозиторий поод проект Tracker

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -4,5 +4,15 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
-
+    @Test
+    void whenAddItem() {
+        String[] answers = {"Fix PC"};
+        Input input = new MockInput(answers);
+        Tracker tracker = new Tracker();
+        CreateAction action = new CreateAction();
+        action.execute(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName()).isEqualTo(expected.getName());
+    }
 }
