172c62388e1207a2ca3812423dfd9529b6ae2df1
user <ruslano48@yandex.ru>
2025-02-18 09:56:20 +0300

    Репозиторий поод проект Tracker

--- src/main/java/ru/job4j/ex/FactorialRecursion.java
+++ src/main/java/ru/job4j/ex/FactorialRecursion.java
@@ -5,9 +5,8 @@ public class FactorialRecursion {
         if (n == 0 || n == 1) {
             return 1;
 
-        } else {
-            return calc(n - 1) * n;
         }
+        return calc(n - 1) * n;
     }
 
     public static void main(String[] args) {
@@ -15,4 +14,5 @@ public class FactorialRecursion {
         System.out.println(result);
     }
 }
+
 
