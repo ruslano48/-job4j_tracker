8ce63df5b4fea43100ead1acfd222d339e6d354d
user <ruslano48@yandex.ru>
2025-03-10 11:53:24 +0300

    Репозиторий поод проект Tracker

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean result = true;
+        String[] origin = originText.split(" ");
+        String[] duplicate = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+
+        for (String word : origin) {
+            check.add(word);
+        }
+
+        for (String word : duplicate) {
+            if (!check.contains(word)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
