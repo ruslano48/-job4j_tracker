60f0f57b853eccbc9bc5163d8ada2e7fb8c7b028
user <ruslano48@yandex.ru>
2025-03-26 12:48:11 +0300

    0. Сортировка [#10096]

---
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        int minLenght = Math.min(left.length(), right.length());
+
+        for (int i = 0; i < minLenght; i++) {
+            int comparison = Character.compare(left.charAt(i), right.charAt(i));
+
+            if (comparison != 0) {
+
+                return comparison;
+            }
+        }
+        return Integer.compare(left.length(), right.length());
+    }
+}
