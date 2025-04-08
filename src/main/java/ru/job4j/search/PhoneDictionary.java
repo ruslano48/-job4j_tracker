d718a9834729e47bc524b30f01bb4b0d8f8b5016
user <ruslano48@yandex.ru>
2025-04-08 11:17:17 +0300

    2.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     ArrayList<Person> persons = new ArrayList<>();
@@ -10,13 +11,19 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> byName = person -> person.getName().contains(key);
+        Predicate<Person> bySurname = person -> person.getSurname().contains(key);
+        Predicate<Person> byPhone = person -> person.getPhone().contains(key);
+        Predicate<Person> byAddress = person -> person.getAddress().contains(key);
+        Predicate<Person> combine = byName
+                .or(bySurname)
+                .or(byPhone)
+                .or(byAddress);
+
         ArrayList<Person> result = new ArrayList<>();
 
         for (Person person : persons) {
-            if (person.getName().contains(key)
-                    || person.getSurname().contains(key)
-                    || person.getPhone().contains(key)
-                    || person.getAddress().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
 
