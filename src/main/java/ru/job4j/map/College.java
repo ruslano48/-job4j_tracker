9300efd1137fea7662b88c0eb54ce14f383607c4
user <ruslano48@yandex.ru>
2025-04-13 16:01:00 +0300

    17. Путь от циклов к Stream API. [#24260]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -1,6 +1,7 @@
 package ru.job4j.map;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 
 public class College {
@@ -11,38 +12,36 @@ public class College {
 
     }
 
-    public Student findByAccount(String account) {
+    public Optional<Student> findByAccount(String account) {
         return students.keySet()
                 .stream()
                 .filter(student -> student.account().equals(account))
-                .findFirst()
-                .orElse(null);
-
+                .findFirst();
     }
 
-    public Subject findBySubjectName(String account, String name) {
-        Student student = findByAccount(account);
-        if (student != null) {
-            return students.get(student)
-                    .stream()
-                    .filter(subject -> subject.name().equals(name))
-                    .findFirst()
-                    .orElse(null);
-        }
-        return null;
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        return findByAccount(account)
+                .flatMap(student -> students.get(student)
+                        .stream()
+                        .filter(subject -> subject.name().equals(name))
+                        .findFirst());
     }
 
     public static void main(String[] args) {
-        Map<Student, Set<Subject>> students = Map.of(new Student("Student", "000001", "201-18-15"),
+        Map<Student, Set<Subject>> students = Map.of(
+                new Student("Student", "000001", "201-18-15"),
                 Set.of(
                         new Subject("Math", 70),
                         new Subject("English", 85)
                 )
         );
+
         College college = new College(students);
-        Student student = college.findByAccount("000001");
-        System.out.println("Найденный студент: " + student);
-        Subject english = college.findBySubjectName("000001", "English");
-        System.out.println("Оценка по найденному предмету: " + english.score());
+
+        college.findByAccount("000001")
+                .ifPresent(student -> System.out.println("Найденный студент: " + student));
+
+        college.findBySubjectName("000001", "English")
+                .ifPresent(subject -> System.out.println("Оценка по найденному предмету: " + subject.score()));
     }
 }
